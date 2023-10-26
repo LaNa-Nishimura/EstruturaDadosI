de23594ae6006d9c624f5f3c1d1593c084aba1d8
Allana Vitória Zucco Paganini <235199@unoesc.lan>
2023-10-25 22:05:30 -0300

    Aula Oito

---
+++ Atividades/LinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿using LinkedList;
+
+Page pageFirst = new Page() {
+    Content = "Atualmente..."
+};
+Page pageSecond = new Page() {
+    Content = "Aplicação..."
+};
+Page pageThird = new Page() {
+    Content = "Muito de..."
+};
+Page pageFourth = new Page() {
+    Content = "Você sabia...", PageNumber = 4
+};
+//Page pageFifth = new Page() {
+//    Content = "Enquanto..."
+//};
+//Page pageSixth = new Page() {
+//    Content = "Você poderia..."
+//};
+
+Page pageFifth = new Page() {
+   Content = "Enquanto...",
+    PageNumber = 5
+};
+
+Page pageSixth = new Page();
+pageSixth.Content = "Você poderia...";
+pageSixth.PageNumber = 6;
+
+var pageSeventh = new Page(); // 2 atributos, ta dentro da classe
+pageSeventh.Content = "Era uma vez...";
+pageSeventh.PageNumber = 7;
+
+LinkedList<Page> pages = new LinkedList<Page>();
+pages.AddLast(pageSecond);
+LinkedListNode<Page> nodePageFourth = pages.AddLast(pageFourth);
+pages.AddLast(pageSixth);
+pages.AddFirst(pageFirst);
+pages.AddBefore(nodePageFourth, pageThird);
+pages.AddAfter(nodePageFourth, pageFifth);
+
+// LIVRO
